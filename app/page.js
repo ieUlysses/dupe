36009bc75dd85adf631a9d691358bf0b58bbe07e
James Conway <71008025+ieUlysses@users.noreply.github.com>
2023-11-18 16:31:33 +0000

    gutted app revisting post planning

--- app/page.js
+++ app/page.js
@@ -1,19 +1,11 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-
-      <div className={styles.center}>
-        <h2 className={inter.className}>
-          Wayhey boi
-        </h2>
+    <>
+      <div>
+        <h1>
+          Landing page
+        </h1>
       </div>
-
-    </main>
+    </>
   )
 }
